f91c4c37a283d90ef69d0054db5226b61d464ccf
nekithockey75 <nikita01051997@gmail.com>
2020-04-01 16:15:32 +0500

    Updates

---
+++ precache-manifest.5a94c843366376c2269b87beb0652824.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b6635c5d316f0448ea5c6f8bbed925",
+    "url": "/dvizh/index.html"
+  },
+  {
+    "revision": "bd0ca590cc904a4c4344",
+    "url": "/dvizh/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d14f6276c2fd19d499a9",
+    "url": "/dvizh/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "bd0ca590cc904a4c4344",
+    "url": "/dvizh/static/js/2.9f630f62.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/dvizh/static/js/2.9f630f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14f6276c2fd19d499a9",
+    "url": "/dvizh/static/js/main.78d4cbb7.chunk.js"
+  },
+  {
+    "revision": "900e7a008fa00b0e50c5",
+    "url": "/dvizh/static/js/runtime-main.c1f1db24.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/dvizh/static/media/persik.4e1ec840.png"
+  }
+]);
