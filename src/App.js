3b232c4318baf06555ab0750b5361158f780d535
nekithockey75 <nikita01051997@gmail.com>
2020-04-04 00:30:40 +0500

    FIX

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import ProjectCardList from './panels/ProjectCardList';
 import { Tabbar, TabbarItem, Epic } from '@vkontakte/vkui';
 
 const client = new ApolloClient({
-	uri: 'https://dvizh-api.herokuapp.com/'
+	uri: 'http://dvizh-api.herokuapp.com/'
 })
 
 const api = new VKMiniAppAPI();
