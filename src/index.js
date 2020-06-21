e1d624b856114878b915daecbe86fd1835bfd7f1
nekithockey75 <dorianmood@163.com>
2020-06-21 21:45:01 +0500

    Firebase set up

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
 import "core-js/features/map";
 import "core-js/features/set";
+
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { RouterProvider } from "react-router5";
-import { Provider } from "react-redux";
+
 import bridge from "@vkontakte/vk-bridge";
+
+import firebase from "firebase/app";
+import "firebase/database";
+
 import App from "./panels/App";
-import createStore from "./store/reducers";
 import configureRouter from "./create-router";
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
+const VK_ID = "secret-vk-id";
 
 const router = configureRouter();
-const store = createStore(router);
 
+// Firebase Config
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+database.ref("events/").on("value", (dataSnapshot) => {
+  console.log(dataSnapshot.val());
+});
+
+// database.ref("events/").push({
+//   description: "Побегаем, начнем с плотинки, потом попьем чаю с пирогами.",
+//   location: {
+//     lat: 56,
+//     lng: 56
+//   },
+//   name: "Пробежка на Плотинке",
+//   price: 0
+// });
 
 router.start(() => {
   ReactDOM.render(
-    <Provider store={store}>
-      <RouterProvider router={router}>
-        <App router={router} />
-      </RouterProvider>
-    </Provider>,
+    <RouterProvider router={router}>
+      <App router={router} />
+    </RouterProvider>,
     document.getElementById("root")
   );
 });
--- src/panels/App.js
+++ src/panels/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "@vkontakte/vkui/dist/vkui.css";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
+import dotenv from "dotenv";
 
 import Meta from "./components/Meta";
 import TabsRoot from "./Main/TabsRoot";
@@ -9,6 +10,8 @@ import TabsRoot from "./Main/TabsRoot";
 const client = new ApolloClient({
   uri: "https://dvizh-api.herokuapp.com/",
 });
+
+dotenv.config();
 
 const App = (props) => {
   return (
