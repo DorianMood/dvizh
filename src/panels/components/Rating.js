dd1c9f110497925368427a60df218579f68adce8
nekithockey75 <dorianmood@163.com>
2020-08-14 01:20:39 +0500

    finish rating later

--- src/panels/components/Rating.js
+++ src/panels/components/Rating.js
@@ -3,43 +3,61 @@ import {
   Div,
   Title
 } from "@vkontakte/vkui";
-import PropTypes from "prop-types";
+import PropTypes, { element } from "prop-types";
+import firebase from "firebase";
+import "firebase/database";
 
 const Rating = (props) => {
 
   const { eventId } = props;
 
+  const database = firebase.database();
+
   const ratingData = [
-    { key: "😊", value: 101 },
-    { key: "😴", value: 11 },
-    { key: "😍", value: 35 },
-    { key: "🤬", value: 4 },
+    { key: "😊", value: [] },
+    { key: "😴", value: [] },
+    { key: "😍", value: [] },
+    { key: "🤬", value: [] },
   ];
 
   const [rating, setRating] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      setRating(ratingData);
+      database.ref(`rating/${eventId}`).on("value", dataSnapshooot => {
+        const fetchedValue = dataSnapshooot.val();
+        const ratingData1 = fetchedValue ? fetchedValue.map(item => {
+          return { key: item.key, value: Array.isArray(item.value) ? item.value.length : 0 };
+        }) : ratingData;
+        setRating(ratingData1);
+      });
     };
     fetchData();
   }, []);
 
-  const onRate = () => {
-    console.log("RATE");
-  }
+  const onRate = (key) => {
+    console.log(rating);
+    console.log(rating.map(item => {
+      return { key: item.key, ids: item.ids }
+    }));
 
-  if (!Array.isArray(rating))
-    return <></>;
+    database.ref(`rating/${eventId}`).set(
+      ratingData.map(item => {
+        return { key: item.key, ids: [1,1] };
+      })
+    );
+  }
 
   if (eventId) {
     console.log("event");
   }
 
+  console.log("RATING", rating == true);
+
   return (
     <Div style={{ display: "flex" }}>
-      {rating.map((item, key) => (
-        <Div key={key} style={{ display: "flex", flexDirection: "column", flex: 1 }} onClick={onRate}>
+      {(rating ? rating : ratingData).map((item, key) => (
+        <Div key={key} style={{ display: "flex", flexDirection: "column", flex: 1 }} onClick={() => onRate(item.key)}>
           <Title level="2" style={{ textAlign: "center" }}>{item.key}</Title>
           <Title level="3" style={{ textAlign: "center" }}>{item.value}</Title>
         </Div>
