22e32b5e79cfc5e9c14800357ed66466842a62b6
nekithockey75 <dorianmood@163.com>
2020-08-31 13:59:45 +0500

    ∞ символ добавлен

--- src/panels/Event/FilterPanel.js
+++ src/panels/Event/FilterPanel.js
@@ -7,7 +7,6 @@ import {
 } from "@vkontakte/vkui";
 
 export const MAX_DISTANCE = 50 * 1000;
-const EARTH_RADIUS = 45 * 1000 * 1000;
 
 const FilterPanel = (props) => {
 
@@ -23,7 +22,7 @@ const FilterPanel = (props) => {
         onChange={(value) => {
           onUpdate({ key: 'location', value: value >= MAX_DISTANCE ? null : value });
         }}
-        top={`В радиусе ${filterValues['location']} м`}
+        top={`В радиусе ${filterValues['location'] === null ? "∞" : filterValues['location']} м`}
       />
       <Cell></Cell><Cell></Cell>
     </FormLayout>
